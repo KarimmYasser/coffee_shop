86db73424ab31cf949a9a4b232088a0fd6d90444
Joseph Sameh <20220099@stud.fci-cu.edu.eg>
2025-08-28 22:01:09 +0300

    Refactor: Update Username Activity UI and add new resources
    This commit refactors the `activity_username.xml` layout to improve its visual appearance and user experience.
    
    Key changes:
    - Rearranged and restyled existing UI elements.
    - Added a new `LinearLayout` to group the username input field and continue button. This layout has a rounded rectangle background with a border.
    - Introduced new string resources for the username input field, labels, and button.
    - Added new font resources (`inter.xml`, `inter_semibold.xml`) and updated `preloaded_fonts.xml` and `font_certs.xml` accordingly.
    - Added a new drawable resource `rounded_rect_with_border.xml` for the new layout background.

--- app/src/main/java/com/example/cofee_shop/presentation/activities/SplashActivity.kt
+++ app/src/main/java/com/example/cofee_shop/presentation/activities/SplashActivity.kt
@@ -5,18 +5,32 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.asLiveData
 import com.example.cofee_shop.R
+import com.example.cofee_shop.presentation.managers.SplashViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 @SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, UsernameActivity::class.java))
-            finish()
-        }, 1500)
+        viewModel.userName.asLiveData().observe(this) { state ->
+            Handler(Looper.getMainLooper()).postDelayed({
+                if (state == null) {
+                    startActivity(Intent(this, UsernameActivity::class.java))
+                } else {
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+                finish()
+            }, 1500)
+        }
     }
 }
